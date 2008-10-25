3663fa0753b68eaeef6f70442e20b6fe79f954f4
Taylor R Campbell <campbell@mumble.net>
2008-10-25 21:54:15 +0000

    Bump version number: 8 -> 9 beta.  Change copying notice.

--- loop.scm
+++ loop.scm
@@ -1,14 +1,42 @@
 ;;; -*- Mode: Scheme -*-
 
-;;;; Extensible Looping Macros, version 8
+;;;; Extensible Looping Macros, version 9 (BETA)
 
-;;; This code is written by Taylor R. Campbell and placed in the Public
-;;; Domain.  All warranties are disclaimed.
-
+;;; Copyright (c) 2008, Taylor R. Campbell
+;;;
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions
+;;; are met:
+;;;
+;;; * Redistributions of source code must retain the above copyright
+;;;   notice, this list of conditions and the following disclaimer.
+;;;
+;;; * Redistributions in binary form must reproduce the above copyright
+;;;   notice, this list of conditions and the following disclaimer in
+;;;   the documentation and/or other materials provided with the
+;;;   distribution.
+;;;
+;;; * Neither the names of the authors nor the names of contributors
+;;;   may be used to endorse or promote products derived from this
+;;;   software without specific prior written permission.
+;;;
+;;; THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS
+;;; OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+;;; WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY
+;;; DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+;;; GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+;;; WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+;;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+;;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 ;;; This is a variation on Alex Shinn's looping macros described in
 ;;; message-id <1157562097.001179.11470@i42g2000cwa.googlegroups.com>.
 ;;; It has diverged substantially from the original macros, and is now
-;;; documented at <http://mumble.net/~campbell/scheme/foof-loop.txt>.
+;;; documented at <http://mumble.net/~campbell/tmp/foof-loop.txt> [for
+;;; the beta period of foof-loop version 9].
 ;;;
 ;;; This file depends on syn-param.scm, also by Taylor R. Campbell, and
 ;;; SRFI 11 (LET-VALUES).  Ideally, the implementation of LET-VALUES
